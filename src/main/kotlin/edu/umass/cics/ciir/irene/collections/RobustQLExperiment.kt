1e173cfef15def688db0a36dbb171a65c48fe8ef
John Foley <jjfoley@smith.edu>
2019-12-31 11:34:14 -0500

    roberta_variants

--- src/main/kotlin/edu/umass/cics/ciir/irene/collections/RobustQLExperiment.kt
+++ src/main/kotlin/edu/umass/cics/ciir/irene/collections/RobustQLExperiment.kt
@@ -195,3 +195,86 @@ object RobustRMTerms {
         File("robust.rmTerms.json").writeText(outJSON.toString())
     }
 }
+
+object RobustWeightedVariants {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val argp = Parameters.parseArgs(args)
+        val qdata = Parameters.parseFile(File("data/robust_short_queries_roberta_base.json"))
+        val titles: Map<String, String> = File(argp.get("queryFile", "/Users/jfoley/code/queries/robust04/rob04.titles.tsv")).smartLines { lines ->
+            lines.associate { line ->
+                val row =  line.trim().split("\t")
+                Pair(row[0], row[1])
+            }
+        }
+        println(qdata);
+
+
+        val queries = qdata.keys.sorted()
+        val sysName = "rm3_roberta_variants"
+        val trecRunOutput = File("data/robust.$sysName.trecrun").smartPrinter()
+
+        IndexParams().apply {
+            withPath(File(argp.get("index", "robust04.irene")))
+        }.openReader().use { index ->
+            index.env.defaultDirichletMu = index.getAverageDL(index.defaultField)
+            index.env.estimateStats = "min"
+
+            for (qid in queries) {
+                if (qid !in titles) {
+                    println("Skipping: ${qid}")
+                    continue
+                }
+                val title = titles[qid]!!
+
+
+                val (time, qres) = timed {
+                    // per-query, assume we're tokenizing the same documents over and over:
+                    val cache = HashMap<Int, BagOfWords>()
+
+                    // variants with RM expansion:
+                    val variations = HashMap<String, QExpr>()
+                    val pair = qdata.getAsList(qid, String::class.java)
+                    val varText = pair[0].split("\t").map {it.trim()}.filter { it.isNotBlank() }
+                    val varWeights = pair[1].split("\t").filter { it.isNotBlank() }.map { it.toDouble() }
+
+                    for ((text, weight) in varText.zip(varWeights)) {
+                        val q = RelevanceExpansionQ(index, QueryLikelihood(index.tokenize(text)), cache=cache) ?: continue
+                        variations[text] = q.weighted(weight)
+                    }
+                    RelevanceExpansionQ(index, QueryLikelihood(index.tokenize(title)), cache=cache)?.let { q ->
+                        variations.put(title, q)
+                    }
+
+                    val sumVarExpr = SumExpr(variations.values.toList())
+                    /*
+                    val results: Map<String, QueryResults> = index.pool(variations, 1000)
+                            .mapValues { (_, td) -> td.toQueryResults(index) }
+
+                    val combSum = TObjectDoubleHashMap<String>()
+                    for (qres in results.values) {
+                        val normScore = qres.map { it.score }.maxmin()
+                        val names = qres.map { it.name }
+                        for ((name, score) in names.zip(normScore)) {
+                            combSum.adjustOrPutValue(name, score, score)
+                        }
+                    }
+                    val docs = ArrayList<SimpleEvalDoc>()
+                    combSum.forEachEntry { name, score ->
+                        docs.add(SimpleEvalDoc(name, -1, score))
+                    }
+                    Ranked.setRanksByScore(docs)
+                    QueryResults(qid, docs.subList(0, minOf(1000, docs.size)))
+                    */
+                    index.search(sumVarExpr, 1000).toQueryResults(index, qid)
+                }
+                qres.outputTrecrun(trecRunOutput, sysName)
+
+                System.out.printf("${qid}\t$sysName-Variants Time: %1.3fs total=%d\n", time, qres.size)
+            }
+        }
+
+        trecRunOutput.close()
+
+    }
+}
